9838bfb34116677e0768884cc4628bf86ab03896
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-10-06 11:15:59 -0700

    added bezier curve example

--- test/build.sh
+++ test/build.sh
@@ -7,4 +7,5 @@ CFLAGS="-Wall -Wextra -std=c99"
 RAYDIR="../lib/raylib/src"
 APPLFLAGS="-framework IOKit -framework OpenGL -framework Cocoa"
 
-$CC $CFLAGS -o build/$1 $1/$1.c -I$RAYDIR -L$RAYDIR -lraylib $APPLFLAGS
+$CC $CFLAGS -c ../src/vector.c -o ../build/vector.o -I../lib
+$CC $CFLAGS -o build/$1 $1/$1.c -I../lib -I$RAYDIR -L$RAYDIR -lraylib $APPLFLAGS ../build/vector.o
